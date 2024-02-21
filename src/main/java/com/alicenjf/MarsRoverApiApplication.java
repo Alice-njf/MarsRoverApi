e2072f784334dd51e30c8c1bdd8b326699d9e40a
Alice-njf <najafialice@gmail.com>
2024-02-22 00:08:17 +0100

    initial commit

---
+++ src/main/java/com/alicenjf/MarsRoverApiApplication.java
@@ -0,0 +1,16 @@
+package com.alicenjf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+public class MarsRoverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alicenjf/MarsRoverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alicenjf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
