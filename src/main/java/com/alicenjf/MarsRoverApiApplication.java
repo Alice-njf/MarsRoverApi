2bbed4e73677533104dd5fd7e9a917c8f8bc94af
Alice-njf <najafialice@gmail.com>
2024-02-25 20:32:57 +0100

    init

--- src/main/java/com/alicenjf/MarsRoverApiApplication.java
+++ src/main/java/com/alicenjf/MarsRoverApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
-public class MarsRoverApiApplication {
+public class MarsRoverApiApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarsRoverApiApplication.class, args);
