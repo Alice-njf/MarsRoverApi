6e55f92d077b7e6e4c361d916b07dbac244e768e
Alice-njf <najafialice@gmail.com>
2024-03-02 22:56:04 +0100

    add

--- src/main/java/com/alicenjf/web/HomeController.java
+++ src/main/java/com/alicenjf/web/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alicenjf.response.MarsRoverApiResponse;
 import com.alicenjf.service.MarsRoverApiService;
@@ -26,5 +28,13 @@ public class HomeController {
 		model.put("name", "Test");
 		model.put("address", "Test Adrress");
 		return "index";
+	}
+	
+	@PostMapping("/")
+	public String postHomeView(ModelMap model, @RequestParam String marsApiRoverData) {
+		MarsRoverApiResponse roverData = roverService.getRoverData(marsApiRoverData);
+		model.put("roverData", roverData);
+		return "index";
+		
 	}
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,7 +3,10 @@ document.querySelectorAll("label[id*='marsApi'").forEach(
 		label.addEventListener('click', function(){
 			const labelId = this.id;
 			const roverId = labelId.split('marsApi')[1];
-			alert(roverId + " is clicked");
+			//alert(roverId + " is clicked");
+			let apiData = document.getElementById('marsApiRoverData');
+			apiData.value = roverId;
+			document.getElementById("frmRoverType").submit();
 			}
 		)
 	}
