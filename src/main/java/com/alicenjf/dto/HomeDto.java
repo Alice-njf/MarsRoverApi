135e5e8fecf4ce39d700c0419f1bc6dd8124b776
Alice-njf <najafialice@gmail.com>
2024-03-08 14:29:28 +0100

    add

--- src/main/java/com/alicenjf/dto/HomeDto.java
+++ src/main/java/com/alicenjf/dto/HomeDto.java
@@ -1,5 +1,28 @@
 package com.alicenjf.dto;
 
-public class HomeDto {
+import org.springframework.web.bind.annotation.RequestParam;
 
+public class HomeDto {
+	String marsApiRoverData;
+	Integer marsSol;
+	Boolean chekBox;
+	public String getMarsApiRoverData() {
+		return marsApiRoverData;
+	}
+	public void setMarsApiRoverData(String marsApiRoverData) {
+		this.marsApiRoverData = marsApiRoverData;
+	}
+	public Integer getMarsSol() {
+		return marsSol;
+	}
+	public void setMarsSol(Integer marsSol) {
+		this.marsSol = marsSol;
+	}
+	public Boolean getChekBox() {
+		return chekBox;
+	}
+	public void setChekBox(Boolean chekBox) {
+		this.chekBox = chekBox;
+	}
+	
 }
